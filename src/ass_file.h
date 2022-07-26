efdecbdb6678b742ad2a2c24a753669954db5ca8
arch1t3cht <arch1t3cht@gmail.com>
2022-07-26 19:55:04 +0200

    Add line folding
    Line folds are managed using metadata of AssDialogue elements, and
    saved in the project properties. They only affect the appearance of the
    subtitle grid, and have no impact on other line operations.

--- src/ass_file.h
+++ src/ass_file.h
@@ -27,6 +27,8 @@
 //
 // Aegisub Project http://www.aegisub.org/
 
+#pragma once
+
 #include "ass_entry.h"
 
 #include <libaegisub/fs.h>
@@ -54,6 +56,13 @@ struct ExtradataEntry {
 	std::string value;
 };
 
+// Both start and end are inclusive
+struct LineFold {
+	int start;
+	int end;
+	bool collapsed;
+};
+
 struct AssFileCommit {
 	wxString const& message;
 	int *commit_id;
@@ -78,11 +87,13 @@ struct ProjectProperties {
 	int active_row = 0;
 	int ar_mode = 0;
 	int video_position = 0;
+	std::vector<LineFold> folds;
 };
 
 class AssFile {
 	/// A set of changes has been committed to the file (AssFile::COMMITType)
 	agi::signal::Signal<int, const AssDialogue*> AnnounceCommit;
+	agi::signal::Signal<int, const AssDialogue*> AnnouncePreCommit;
 	agi::signal::Signal<AssFileCommit> PushState;
 public:
 	/// The lines in the file
@@ -178,8 +189,11 @@ public:
 		COMMIT_DIAG_FULL   = COMMIT_DIAG_META | COMMIT_DIAG_TIME | COMMIT_DIAG_TEXT,
 		/// Extradata entries were added/modified/removed
 		COMMIT_EXTRADATA   = 0x100,
+		/// Folds were added or removed
+		COMMIT_FOLD        = 0x200,
 	};
 
+	DEFINE_SIGNAL_ADDERS(AnnouncePreCommit, AddPreCommitListener)
 	DEFINE_SIGNAL_ADDERS(AnnounceCommit, AddCommitListener)
 	DEFINE_SIGNAL_ADDERS(PushState, AddUndoManager)
 
